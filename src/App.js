5807000b073e67fcaedfea9f013686ba8c11dc15
billonline33 <billonline33@hotmail.com>
2018-08-27 23:41:05 +1000

    user interface: create app component and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import logo from './images/logo.png';
+import tile from './images/tile.jpg';
+import './css/style.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="banner">
+        <div className="banner-container">
+          <div className="banner-image">
+            <img src={tile} alt="tile" />
+            <a href="#" className="banner-link">
+              <img src={logo} alt="logo" />
+              <p>Home and Away</p>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
