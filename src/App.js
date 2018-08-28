cdb313e17b2ff84f5024fa4819fe398d3592300a
billonline33 <billonline33@hotmail.com>
2018-08-28 20:44:27 +1000

    style: install styled-component and creaete BannerContainer
    npm install --save styled-components
    create BannerContainer and replace div element

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import logo from './images/logo.png';
 import tile from './images/tile.jpg';
 import './css/style.css';
+import styled from 'styled-components';
+
+const BannerContainer = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  background-color: #000000;
+  margin: 0 auto;
+  padding: 50px;
+  height: 100vh;
+  width: 1200px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="banner">
-        <div className="banner-container">
-          <div className="banner-image">
-            <img src={tile} alt="tile" />
-            <a href="#" className="banner-link">
-              <img src={logo} alt="logo" />
-              <p>Home and Away</p>
-            </a>
-          </div>
+      <BannerContainer>
+        <div className="banner-image">
+          <img src={tile} alt="tile" />
+          <a className="banner-link">
+            <img src={logo} alt="logo" />
+            <p>Home and Away</p>
+          </a>
         </div>
-      </div>
+      </BannerContainer>
     );
   }
 }
