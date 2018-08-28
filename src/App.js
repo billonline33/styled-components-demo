047fae34db21e00c5deac63a3545ce9a531cc90a
billonline33 <billonline33@hotmail.com>
2018-08-28 21:00:02 +1000

    style: create BannerImage and BannerLink and

--- src/App.js
+++ src/App.js
@@ -13,17 +13,51 @@ const BannerContainer = styled.div`
   width: 1200px;
 `;
 
+const BannerImage = styled.div`
+  position: relative;
+  max-width: 500px;
+
+  > img {
+    width: 100%;
+  }
+`;
+
+const BannerLink = styled.a`
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  text-decoration: none;
+  height: calc(100% / 3);
+  width: 100%;
+
+  img {
+    margin: 15px;
+    height: calc(100% - 30px);
+  }
+
+  p {
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold;
+  }
+`;
+
 class App extends Component {
   render() {
     return (
       <BannerContainer>
-        <div className="banner-image">
+        <BannerImage>
           <img src={tile} alt="tile" />
-          <a className="banner-link">
+          <BannerLink>
             <img src={logo} alt="logo" />
             <p>Home and Away</p>
-          </a>
-        </div>
+          </BannerLink>
+        </BannerImage>
       </BannerContainer>
     );
   }
